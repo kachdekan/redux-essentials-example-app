d2e25d8522366232bda5b7ce2344249f47998482
Kachisa D.N <kachdekan@gmail.com>
2022-09-01 12:18:15 +0300

    sorting the posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,9 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
+  { id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString() },
+  { id: '2', title: 'Second Post', content: 'More text', date: sub(new Date(), { minutes: 5 }).toISOString() }
 ]
 
 const postsSlice = createSlice({
@@ -19,7 +20,8 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
-            user: userId
+            user: userId,
+            date: new Date().toISOString()
           }
         }
       }
