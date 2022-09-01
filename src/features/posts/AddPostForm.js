ed5110da7390e3ae7f01e47b4d0ee4e14f340b2b
Kachisa D.N <kachdekan@gmail.com>
2022-09-01 10:26:52 +0300

    Added the add post functionality

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { nanoid } from "@reduxjs/toolkit"
+import { useDispatch } from 'react-redux'
+import { postAdded } from './postsSlice'
+
+export const AddPostForm = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const dispatch = useDispatch()
+
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onContentChanged = e => setContent(e.target.value)
+
+  const handlePostSaved = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content
+        })
+      )
+
+      setTitle('')
+      setContent('')
+    }
+  }
+
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={handlePostSaved}>Save Post</button>
+      </form>
+    </section>
+  )
+}
